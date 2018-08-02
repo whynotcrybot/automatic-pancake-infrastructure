fdc996645c13d756a50147f50dd749f9c7841e61
whynotcrybot <alexanderkatsero@gmail.com>
2018-08-02 10:29:22 +0300

    add cookbook and recipes to install git, nodejs and start the app

--- cookbooks/my-first-cookbook/recipes/default.rb
+++ cookbooks/my-first-cookbook/recipes/default.rb
@@ -4,4 +4,26 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
+include_recipe "git"
 include_recipe "nodejs"
+
+git "/tmp/app" do
+  repository "git://github.com/whynotcrybot/automatic-pancake"
+  reference "master"
+  action :sync
+end
+
+execute 'npm i' do
+  cwd '/tmp/app'
+  command 'npm i'
+end
+
+#execute 'start node server' do
+  #command 'node /tmp/app/server.js'
+#end
+
+service "myapp_service" do
+  supports :start => true
+  start_command "node /tmp/app/server.js"
+  action [ :start ]
+end
